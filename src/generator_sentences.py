140a10b97ff599341cb914986c7ed40621b7be45
FriLDD <dotpointen@gmail.com>
2021-02-10 19:46:18 +0300

    Getting the random value of their dictionary.

--- src/generator_sentences.py
+++ src/generator_sentences.py
@@ -1,19 +1,15 @@
 
 # Markov chain algorithm
-from pprint import pprint
-from random import choice
-from random import randint
-from time import time
+# from pprint import pprint
+from random import choice, randint
+# from time import time
 
 
-start = time()
 with open("../data/data.txt", "r") as file:
     text = file.read()
     file.close()
-text = text.lower()
-text = text.replace("\n", "")
-text = text.replace(".", "")
-text = text.split(" ")
+
+text = text.lower().replace("\n", "").replace(".", "").split(" ")
 
 test_dict = {}
 for i in range(len(text) - 1):
@@ -21,16 +17,14 @@ for i in range(len(text) - 1):
         test_dict[text[i]] = [text[i+1]]
     else:
         test_dict[text[i]].append(text[i + 1])
-print(time() - start)
+
 
 out = []
-value = "в"
+value = choice(list(test_dict.keys()))
 for i in range(randint(5, 30)):
     value = choice(test_dict[value])
     out.append(value)
 
-
 out = ' '.join(out)
 out = out.capitalize()
 print(out)
-print(test_dict.__sizeof__())
