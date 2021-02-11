d61c7efd2a3cd88470a3c9238e07e754a3d7db8d
FriLDD <dotpointen@gmail.com>
2021-02-11 21:35:56 +0300

    Guaranteed return of a non-empty message.

--- src/generator_sentences.py
+++ src/generator_sentences.py
@@ -8,28 +8,29 @@ import re
 with open("D:/Projects/PythonProjects/Vk_Bot_ILLO/data/test.txt", "r") as file:
     text_source = file.read()
     file.close()
+start = time()
+text = re.sub(r"[.,?\n!():—-]", '', text_source).lower().split(" ")
 
-text = re.sub(r"[,.?\n!():—-]", '', text_source).lower().split(" ")
 
 test_dict = {}
 i = 0
 for i in range(len(text) - 1):
-    # print(text[i])
     if text[i] not in test_dict:
         test_dict[text[i]] = [text[i + 1]]
     else:
         test_dict[text[i]].append(text[i + 1])
 test_dict[text[i + 1]] = [text[i + 1]]
 
+
 out = []
 value = choice(list(test_dict.keys()))
 for i in range(randint(50, 70)):
     if [value] == test_dict[value]:
+        out.append(value)
         break
     value = choice(test_dict[value])
     out.append(value)
 
 
-out = ' '.join(out) + ' '
-out = out.capitalize()
+out = (' '.join(out)).capitalize()
 print(out)
