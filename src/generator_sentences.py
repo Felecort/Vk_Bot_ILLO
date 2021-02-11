7f8bb41ef915b1ba781c327fc0766fe9a203ea3d
FriLDD <dotpointen@gmail.com>
2021-02-11 21:15:44 +0300

    Improved algorithm for generating sentences.

--- src/generator_sentences.py
+++ src/generator_sentences.py
@@ -1,30 +1,36 @@
-
 # Markov chain algorithm
 # from pprint import pprint
 from random import choice, randint
-# from time import time
+from pprint import pprint
+from time import time
+import re
 
-
-with open("../data/data.txt", "r") as file:
-    text = file.read()
+with open("D:/Projects/PythonProjects/Vk_Bot_ILLO/data/test.txt", "r") as file:
+    text_source = file.read()
     file.close()
 
-text = text.lower().replace("\n", "").replace(".", "").split(" ")
+text = re.sub(r"[,.?\n!():—-]", '', text_source).lower().split(" ")
+print(text)
 
 test_dict = {}
 for i in range(len(text) - 1):
     if text[i] not in test_dict:
-        test_dict[text[i]] = [text[i+1]]
+        test_dict[text[i]] = [text[i + 1]]
     else:
         test_dict[text[i]].append(text[i + 1])
 
-
+pprint(test_dict)
 out = []
 value = choice(list(test_dict.keys()))
-for i in range(randint(5, 30)):
+for i in range(randint(7, 20)):
+    if value not in test_dict:
+        break
     value = choice(test_dict[value])
     out.append(value)
+# pprint(test_dict)
 
-out = ' '.join(out)
+
+out = ' '.join(out) + ' '
 out = out.capitalize()
+print()
 print(out)
