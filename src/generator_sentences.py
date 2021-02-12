0f9cd6263e206b7cd62460748ffcab521bca881e
FriLDD <dotpointen@gmail.com>
2021-02-12 15:38:58 +0300

    Updated and moved the add to dictionary functions.

--- src/generator_sentences.py
+++ src/generator_sentences.py
@@ -1,19 +1,6 @@
 
 # Markov chain algorithm
 from random import choice, randint
-import re
-
-
-def add_dict(new_text, data_dict):
-    new_text = re.sub(r"[.,?\n!():—-]", '', new_text).lower().split(" ")
-    i = 0
-    for i in range(len(new_text) - 1):
-        if new_text[i] not in data_dict:
-            data_dict[new_text[i]] = [new_text[i + 1]]
-        else:
-            data_dict[new_text[i]].append(new_text[i + 1])
-    data_dict[new_text[i + 1]] = [new_text[i + 1]]
-    return data_dict
 
 
 def generate_message(data_dict):
--- src/handler_database.py
+++ src/handler_database.py
@@ -1,6 +1,7 @@
 
 from pprint import pprint
 import json
+import re
 data_filename = "../data/database.json"
 
 
@@ -13,3 +14,22 @@ def get_database():
 def set_database(database):
     with open(data_filename, "w") as file:
         json.dump(database, file, indent=4, ensure_ascii=False)
+
+
+def add_to_dict(new_text, database, chat_id):
+    chat_id = str(chat_id)
+    new_text = re.sub(r"[.,?\n!():—-]", '', new_text).lower().split(" ")
+    i = 0
+    # database["chat_id"]["markov_chains"]
+    for i in range(len(new_text) - 1):
+        if new_text[i] not in database[chat_id]["markov_chains"]:
+            database[chat_id]["markov_chains"][new_text[i]] = [new_text[i + 1]]
+        else:
+            database[chat_id]["markov_chains"][new_text[i]].append(new_text[i + 1])
+    database[chat_id]["markov_chains"][new_text[i + 1]] = [new_text[i + 1]]
+    return database
+
+
+data = get_database()
+data = add_to_dict("привет, меня зовут олег, а тебя как зовут Ыгрь", data, 1)
+set_database(data)
