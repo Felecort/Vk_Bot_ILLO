f8c39fd4e09de3b63331fa90f4f2b810759b6b39
FriLDD <dotpointen@gmail.com>
2021-02-12 20:45:28 +0300

    Improved the character filter.

--- src/handler_database.py
+++ src/handler_database.py
@@ -1,9 +1,5 @@
 
-from pprint import pprint
 import json
-import re
-import sys
-# data_filename = sys.path[1] + "\\data\\database.json"
 data_filename = "D:\\Projects\\PythonProjects\\Vk_Bot_ILLO\\data\\database.json"
 
 
@@ -20,19 +16,23 @@ def set_database(database):
 
 def add_to_dict(new_text, database, chat_id):
     chat_id = str(chat_id)
-    new_text = re.sub(r"[.,?\n!():—-]", '', new_text).lower().split(" ")
+
+    new_text = "".join(c for c in new_text if c.isalpha() or c == " " or c.isdigit()).lower().split()
+
+    print(new_text)
     i = 0
-    # database["chat_id"]["markov_chains"]
     for i in range(len(new_text) - 1):
         if new_text[i] not in database[chat_id]["markov_chains"]:
             database[chat_id]["markov_chains"][new_text[i]] = [new_text[i + 1]]
         else:
             database[chat_id]["markov_chains"][new_text[i]].append(new_text[i + 1])
     database[chat_id]["markov_chains"][new_text[i + 1]] = [new_text[i + 1]]
+
     set_database(database)
+    # print(database)
     return database
 
 
 # data = get_database()
-# data = add_to_dict("привет, меня зовут олег, а тебя как зовут Ыгрь", data, 1)
+# data = add_to_dict("😜 привет 🗿😜 я камень!", data, 1)
 # set_database(data)
